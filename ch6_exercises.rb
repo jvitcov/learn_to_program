255c32f034159eb0771e8b172ecc20ef71d99c5b
Justin Vitcov <vitcovja@gmail.com>
2014-05-01 20:27:07 -0700

    Chapter 6

---
+++ ch6_exercises.rb
@@ -0,0 +1,5 @@
+puts 'Get ready, here comes your angry boss!'
+puts '...'
+puts 'Angry Boss: "What do you want?"'
+request = gets.chomp
+puts 'Angry Boss: "You\'ve got some nerve asking for '+request.upcase+'. YOU\'RE FIRED!"'
